c046179964cbd998ad1876d4e5fa29903a255ba0
VonRiddarn <vonriddarn@gmail.com>
2025-02-21 10:38:39 +0100

    feat: generated fruit database

---
+++ src/data/fruits.ts
@@ -0,0 +1,104 @@
+type Food = {
+	image:
+		| "🍇"
+		| "🍈"
+		| "🍉"
+		| "🍊"
+		| "🍋"
+		| "🍋‍🟩"
+		| "🍐"
+		| "🍏"
+		| "🍎"
+		| "🥭"
+		| "🍍"
+		| "🍌"
+		| "🍑"
+		| "🍒"
+		| "🍓"
+		| "🥝";
+	name: string;
+	stock: number;
+};
+
+export const foodDb: Food[] = [
+	{
+		image: "🍇",
+		name: "Grapes",
+		stock: 10,
+	},
+	{
+		image: "🍈",
+		name: "Melon",
+		stock: 15,
+	},
+	{
+		image: "🍉",
+		name: "Watermelon",
+		stock: 8,
+	},
+	{
+		image: "🍊",
+		name: "Tangerine",
+		stock: 20,
+	},
+	{
+		image: "🍋",
+		name: "Lemon",
+		stock: 25,
+	},
+	{
+		image: "🍋‍🟩",
+		name: "Lime",
+		stock: 18,
+	},
+	{
+		image: "🍐",
+		name: "Pear",
+		stock: 12,
+	},
+	{
+		image: "🍏",
+		name: "Green Apple",
+		stock: 30,
+	},
+	{
+		image: "🍎",
+		name: "Red Apple",
+		stock: 30,
+	},
+	{
+		image: "🥭",
+		name: "Mango",
+		stock: 10,
+	},
+	{
+		image: "🍍",
+		name: "Pineapple",
+		stock: 5,
+	},
+	{
+		image: "🍌",
+		name: "Banana",
+		stock: 40,
+	},
+	{
+		image: "🍑",
+		name: "Peach",
+		stock: 15,
+	},
+	{
+		image: "🍒",
+		name: "Cherries",
+		stock: 25,
+	},
+	{
+		image: "🍓",
+		name: "Strawberry",
+		stock: 35,
+	},
+	{
+		image: "🥝",
+		name: "Kiwi",
+		stock: 20,
+	},
+];
