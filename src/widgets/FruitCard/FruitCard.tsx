a32ddccc65ffb4f2fd55c3881b36295f73e5c9ad
VonRiddarn <vonriddarn@gmail.com>
2025-02-21 11:34:26 +0100

    feat: added sell button to fruit cards that uses state

--- src/widgets/FruitCard/FruitCard.tsx
+++ src/widgets/FruitCard/FruitCard.tsx
@@ -1,16 +1,29 @@
+import React from "react";
 import { Fruit } from "../../data/fruitsDb";
 import "./FruitCard.scss";
+import Button from "../../components/Button/Button";
 
 interface FruitCardProps {
 	fruit: Fruit;
 }
 
-const FruitCard = ({ fruit }: FruitCardProps) => (
-	<article className="FruitCard">
-		<h2>{fruit.image}</h2>
-		<h3>{fruit.name}</h3>
-		<p>Stock: {fruit.stock}</p>
-	</article>
-);
+const FruitCard = ({ fruit }: FruitCardProps) => {
+	const [stock, setStock] = React.useState(fruit.stock);
+
+	return (
+		<article className="FruitCard">
+			<h2>{fruit.image}</h2>
+			<h3>{fruit.name}</h3>
+			<p>Stock: {stock}</p>
+			<Button
+				title="Sell"
+				onClick={() => {
+					if (stock <= 0) return;
+					setStock(stock - 1);
+				}}
+			/>
+		</article>
+	);
+};
 
 export default FruitCard;
