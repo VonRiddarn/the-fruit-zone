e168adf9a8a1c8581dd2c103741bbd4fb01cf28e
HanninenF <hanninenfredrik@gmail.com>
2025-02-24 23:04:29 +0100

    add: searchfunction

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,34 @@ import FruitCard from "./widgets/FruitCard/FruitCard";
 import { Fruit } from "./types/Fruit";
 
 function App() {
-	const [fruits, setFruits] = useState<Fruit[]>(fruitsDb);
+  const [fruits, setFruits] = useState<Fruit[]>(fruitsDb);
+  const [filteredFruits, setFilteredFruits] = useState<Fruit[]>(fruits);
 
-	const removeFruit = (id: number) => {
-		setFruits((prevFruits) => prevFruits.filter((fruit) => fruit.id !== id));
-	};
+  const removeFruit = (id: number) => {
+    setFruits((prevFruits) => prevFruits.filter((fruit) => fruit.id !== id));
+  };
 
-	return (
-		<>
-			<h1 className="unselectable">
-				The <span>fruit</span> zone
-			</h1>
-			<div className="FruitContainer">
-				{fruits.map((fruit) => (
-					<FruitCard key={fruit.name} fruit={fruit} onRemove={removeFruit} />
-				))}
-			</div>
-		</>
-	);
+  const SearchFruit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value.toLowerCase();
+
+    setFilteredFruits(
+      fruits.filter((fruit) => fruit.name.toLowerCase().startsWith(searchTerm))
+    );
+  };
+
+  return (
+    <>
+      <h1 className="unselectable">
+        The <span>fruit</span> zone
+      </h1>
+      <input type="text" onChange={SearchFruit} />
+      <div className="FruitContainer">
+        {filteredFruits.map((fruit) => (
+          <FruitCard key={fruit.name} fruit={fruit} onRemove={removeFruit} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
